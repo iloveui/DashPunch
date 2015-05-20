d4d16c443b3ffc5d79c15a31cacae1ec628e58b6
Ui <uimail@gmail.com>
2015-05-20 00:51:53 -0430

    no message

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+	public Vector3 targetVector;
+
+	public float speed=10.0f;
+	public bool moveForward;
+	public bool moveBackward;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(moveBackward){
+			//no he llegado al target
+			if((targetVector.x-this.transform.localPosition.x)>0.1f){
+				Vector3 tempPos=this.transform.localPosition;
+				tempPos.x= tempPos.x + speed*Time.deltaTime;
+				this.transform.localPosition=tempPos;
+				//SI ME PASE
+				if(this.transform.localPosition.x>=targetVector.x){
+					Vector3 correctPos=this.transform.localPosition;
+					correctPos.x= targetVector.x;
+					this.transform.localPosition=correctPos;
+					targetVector.x=targetVector.x - 25f;
+					moveForward=true;
+					moveBackward=false;
+				}
+			}
+		}
+		else if(moveForward){
+			//no he llegado al target
+			if((this.transform.localPosition.x - targetVector.x)>0.1f){
+				Vector3 tempPos=this.transform.localPosition;
+				tempPos.x= tempPos.x - speed*Time.deltaTime;
+				this.transform.localPosition=tempPos;
+				//SI ME PASE
+				if(this.transform.localPosition.x<=targetVector.x){
+					Vector3 correctPos=this.transform.localPosition;
+					correctPos.x= targetVector.x;
+					this.transform.localPosition=correctPos;
+					moveBackward=true;
+				}
+			}
+		}
+	}
+	public void OnMoveForward(){
+		targetVector=this.transform.localPosition;
+		targetVector.x=targetVector.x + 50.0f;
+		moveBackward=true;
+		moveForward=false;
+	}
+}
--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
+public class Player2 : MonoBehaviour {
 
 	public Vector3 targetVector;
 
@@ -11,34 +11,29 @@ public class Move : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
-
-
 
 	// Update is called once per frame
 	void Update () {
 
-		if(moveForward){
+		if(moveBackward){
 			//no he llegado al target
 			if((targetVector.x-this.transform.localPosition.x)>0.1f){
 				Vector3 tempPos=this.transform.localPosition;
 				tempPos.x= tempPos.x + speed*Time.deltaTime;
-				this.transform.localPosition=tempPos; 
-
+				this.transform.localPosition=tempPos;
 				//SI ME PASE
 				if(this.transform.localPosition.x>=targetVector.x){
 					Vector3 correctPos=this.transform.localPosition;
 					correctPos.x= targetVector.x;
 					this.transform.localPosition=correctPos;
 					targetVector.x=targetVector.x - 25f;
-					moveForward=false;
-					moveBackward=true;
-
+					moveForward=true;
+					moveBackward=false;
 				}
 			}
 		}
-		else if(moveBackward){
+		else if(moveForward){
 			//no he llegado al target
 			if((this.transform.localPosition.x - targetVector.x)>0.1f){
 				Vector3 tempPos=this.transform.localPosition;
@@ -49,30 +44,15 @@ public class Move : MonoBehaviour {
 					Vector3 correctPos=this.transform.localPosition;
 					correctPos.x= targetVector.x;
 					this.transform.localPosition=correctPos;
-					moveBackward=false;
+					moveBackward=true;
 				}
 			}
 		}
-
 	}
-
-
 	public void OnMoveForward(){
 		targetVector=this.transform.localPosition;
 		targetVector.x=targetVector.x + 50.0f;
-		moveBackward=false;
-		moveForward=true;
-
-
+		moveBackward=true;
+		moveForward=false;
 	}
-
-
-
-
-
-
-
-
-
-
 }
